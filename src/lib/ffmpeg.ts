108739f3b7d8b0d005dd9e624e9f1e9f7e0422dd
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2023-09-17 23:43:37 -0300

    tsconfig

--- src/lib/ffmpeg.ts
+++ src/lib/ffmpeg.ts
@@ -1,31 +1,27 @@
-import { FFmpeg } from '@ffmpeg/ffmpeg'
-import { toBlobURL } from '@ffmpeg/util'
+import { FFmpeg } from "@ffmpeg/ffmpeg";
 
+import coreURL from "../ffmpeg/ffmpeg-core.js?url";
+import wasmURL from "../ffmpeg/ffmpeg-core.wasm?url";
+import workerURL from "../ffmpeg/ffmpeg-worker.js?url";
 
-let ffmpeg: FFmpeg | null
+let ffmpeg: FFmpeg | null;
 
 export async function getFFmpeg() {
-    if (ffmpeg) {
-        return ffmpeg
-    }
+  if (ffmpeg) {
+    return ffmpeg;
+  }
 
-    ffmpeg = new FFmpeg()
+  ffmpeg = new FFmpeg();
 
-    const baseURL = "https://unpkg.com/@ffmpeg/core-mt@0.12.2/dist/esm";
+  if (!ffmpeg.loaded) {
+    await ffmpeg.load({
+      coreURL,
+      wasmURL,
+      workerURL,
+    });
+  }
 
-    if (!ffmpeg.loaded) {
-        await ffmpeg.load({
-            coreURL: await toBlobURL(`${baseURL}/ffmpeg-core.js`, 'text/javascript'),
-            wasmURL: await toBlobURL(`${baseURL}/ffmpeg-core.wasm`, 'application/wasm'),
-            workerURL: await toBlobURL(
-                `${baseURL}/ffmpeg-core.worker.js`,
-                "text/javascript"
-              ),
-            
-        })
-    }
-    
-    return ffmpeg
+  return ffmpeg;
 }
 
 
