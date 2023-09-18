99f2317be84f495f7adf441672caf9107967e3e9
Beatriz Oliveira Ferreira <beaferreira.trabalho@gmail.com>
2023-09-18 14:12:38 -0300

    finalizado

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,33 @@
-import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import path from "path"
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
  
+// export default defineConfig({
+//   server: {
+//     headers: {
+//       'Cross-Origin-Embedder-Policy': 'require-corp',
+//       'Cross-Origin-Opener-Policy': 'same-origin',
+//     },
+//   },
+//   plugins: [react()],
+//   optimizeDeps: {
+//     exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+import { defineConfig } from 'vite'
+import path from 'path'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  server: {
-    headers: {
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-      'Cross-Origin-Opener-Policy': 'same-origin',
-    },
-  },
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util']
+    exclude: ['@ffmpeg/ffmpeg','@ffmpeg/util']
   },
   resolve: {
     alias: {
